911251e3592a719fb4cdf89dee79b46a3110b4e3
NDseah <ndseah95@gmail.com>
2018-10-23 18:41:22 +0800

    Added view and controller for displaying a list of rooms

--- lib/learn_phoenix_web/router.ex
+++ lib/learn_phoenix_web/router.ex
@@ -16,7 +16,7 @@ defmodule LearnPhoenixWeb.Router do
   scope "/", LearnPhoenixWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", RoomController, :index
   end
 
   # Other scopes may use custom stacks.
